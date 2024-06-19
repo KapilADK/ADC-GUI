71ce43d1c59cee618b0dcf7b6d7577ca948df3d2
KapilADK <153365734+KapilADK@users.noreply.github.com>
2024-06-19 20:37:20 +0200

    Update AdcReceiver.py

--- AdcReceiver.py
+++ AdcReceiver.py
@@ -2,38 +2,56 @@ from PyQt6.QtCore import QThread, pyqtSignal
 import time
 
 from rp.core import RedPitayaBoard
-from source import *
-from rp.constants import ALL_BRAM_DAC_PORTS
+from source import initAdcReceiver
 
+DEBUG_MODE = False
 VERBOSE = False
+autoStartServer = False
+
+pita = RedPitayaBoard(
+    debug=DEBUG_MODE, verbose=VERBOSE, autoStartServer=autoStartServer
+)
 
 class AdcReceiverThread(QThread):
     dataReceived = pyqtSignal(bytes)
-    pita = RedPitayaBoard(verbose = VERBOSE)
 
-    def __init__(self, sample_rate, signal, start_V, stop_V):
+    def __init__(self):
         super().__init__()
         self.running = False
-        self.sample_rate = sample_rate
-        self.signal = signal
-        self.start_V = start_V
-        self.stop_V = stop_V
+        self.sample_rate = None
+        self.mode = None
+        self.dac_steps = None
+        self.dwell_time_ms = None
+
+    def set_parameters(self, sample_rate, mode, dac_steps, dwell_time_ms):
+        self.sample_rate, self.mode, self.dac_steps, self.dwell_time_ms = (
+            sample_rate, mode, dac_steps, dwell_time_ms
+        )
 
     def run(self):
         self.running = True
-        self.config = initMeasurement(self.pita, self.sample_rate, self.signal, self.start_V, self.stop_V,VERBOSE)
-        self.number_tcp_pkg = self.config["adc"]["tcp"]
-        self.tcp_pkg_size_bytes = self.config["ram"].tcp_pkg_size_bytes
-        self.pita.start_dac_sweep(port = ALL_BRAM_DAC_PORTS)
-        
+        self.config_adc = initAdcReceiver(
+            pita,
+            self.sample_rate,
+            self.mode,
+            self.dac_steps,
+            self.dwell_time_ms,
+            VERBOSE,
+        )
+        self.number_tcp_pkg = self.config_adc["adc"]["tcp"]
+        self.tcp_pkg_size_bytes = self.config_adc["ram"].tcp_pkg_size_bytes
+
         while self.running:
-                self.pita.start_adc_sampling(self.number_tcp_pkg)
-                index = 0
-                while index < self.number_tcp_pkg:
-                    receive_start = time.time()
-                    data_raw = self.pita.receive_adc_data_package(2*int(self.tcp_pkg_size_bytes))
-                    receive_end = time.time()
-                    print(f"Data received in {receive_end-receive_start} s")
-                    self.dataReceived.emit(data_raw)
-                    index += 2
-                    time.sleep(0.1)
+            pita.start_adc_sampling(self.number_tcp_pkg)
+            index = 0
+            while index < self.number_tcp_pkg:
+                data_raw = pita.receive_adc_data_package(
+                    2 * int(self.tcp_pkg_size_bytes)
+                )
+                self.dataReceived.emit(data_raw)
+                index += 2
+                time.sleep(0.1)
+
+    def stop(self):
+        self.running = False
+        self.wait()
