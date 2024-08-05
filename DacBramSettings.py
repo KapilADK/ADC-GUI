fe00b742813d5b1fc27181b3e3b7c2418364828a
KapilADK <153365734+KapilADK@users.noreply.github.com>
2024-08-05 12:30:14 +0200

    Update DacBramSettings.py

--- DacBramSettings.py
+++ DacBramSettings.py
@@ -9,17 +9,16 @@ from PyQt6.QtWidgets import (
     QLineEdit,
     QLabel,
     QRadioButton,
-    QApplication,
 )
 from PyQt6.QtGui import QDoubleValidator
 from PyQt6.QtCore import pyqtSlot, pyqtSignal
-import sys
-import time
+
 
 from source import initDacBram, stop_sweep
-from AdcReceiver import pita
+from AdcReceiver import pita, AdcReceiverThread
+from SignalManager import DacSignalManager
 
-MHZ_MAX_DAC_FREQ = 25
+MHZ_MAX_DAC_FREQ = 125
 
 
 class StartStopButton(QPushButton):
@@ -40,9 +39,9 @@ class StartStopButton(QPushButton):
 
 
 class DacBramSettingsTab(QGroupBox):  # QGroupBox
-    restart_adc = pyqtSignal()
+    update_adc = pyqtSignal()
 
-    def __init__(self, adcreceiver, channel):
+    def __init__(self, adcreceiver: AdcReceiverThread, channel):
         super().__init__()
 
         # adcReceiverObject:
@@ -53,12 +52,26 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
 
         # default object variables for config:
         self.dwell_time_ms = "4e-05"
-        self.dac_steps = 10
+        self.dac_steps = 1000
         self.signal_type = "Sine"
         self.start_V = 0
         self.stop_V = 1
         self.amplitude = 1
         self.reset_voltage = 0
+
+        self.setupUI()
+
+        # Initialise SignalManager to connect signals and slots
+        self.DacSignalManager = DacSignalManager()
+        self.DacSignalManager.connect_signals(self)
+
+        # Display settings as soon as the GUI starts
+        self.update_signal_options()
+        self.update_frequency_options()
+        self.get_frequency_info()
+        self.show_valid_signal_frequency()
+
+    def setupUI(self):
 
         self.port_layout = QHBoxLayout()
         self.setLayout(self.port_layout)
@@ -139,7 +152,10 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
         self.dac_sample_rate_button = QRadioButton("DAC Sample rate")
         self.dac_sample_rate_button.setChecked(True)
         self.dac_sample_rate_combobox = QComboBox()
-        dac_sample_rates = [
+        self.dac_sample_rates = [
+            125,
+            62.5,
+            31.25,
             25,
             15.625,
             12.5,
@@ -152,9 +168,9 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
             1.25,
             1,
             0.625,
-            0.5,
+            0.5
         ]
-        for dac_sample_rate in dac_sample_rates:
+        for dac_sample_rate in self.dac_sample_rates:
             self.dac_sample_rate_combobox.addItem(f"{dac_sample_rate} MHz")
         self.dac_steps_label = QLabel("DAC Steps")
         self.dac_steps_edit = QLineEdit()
@@ -173,35 +189,12 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
         self.signal_frequency_layout = QHBoxLayout()
         self.signal_frequency_button = QRadioButton("Signal Frequency")
         self.signal_frequency_combobox = QComboBox()
-        frequencies = [
-            2.5,
-            1.25,
-            625,
-            500,
-            312.5,
-            250,
-            156.3,
-            125,
-            100,
-            78.1,
-            62.5,
-            50,
-            25,
-            20,
-            12.5,
-            10,
-        ]
-        for MHz_frequency in frequencies[:2]:
-            self.signal_frequency_combobox.addItem(f"{MHz_frequency} MHz")
-        for KHz_frequencies in frequencies[2:]:
-            self.signal_frequency_combobox.addItem(f"{KHz_frequencies} KHz")
-        self.max_dac_steps_label = QLabel("Max. DAC steps:")
-        self.show_max_dac_steps_label = QLabel()
+
+        self.max_dac_steps_label = QLabel(f"Max. DAC steps:\t16000")
 
         self.signal_frequency_layout.addWidget(self.signal_frequency_button)
         self.signal_frequency_layout.addWidget(self.signal_frequency_combobox)
         self.signal_frequency_layout.addWidget(self.max_dac_steps_label)
-        self.signal_frequency_layout.addWidget(self.show_max_dac_steps_label)
 
         self.frequency_box_layout.addLayout(self.dac_sample_rate_layout)
         self.frequency_box_layout.addLayout(self.signal_frequency_layout)
@@ -211,63 +204,18 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
         self.port_layout.addWidget(self.signal_box)
         self.port_layout.addWidget(self.frequency_box)
 
-        # Connect signals to slots
-        #####################################################################
-        self.start_sweep_button.clicked.connect(self.sweep_button_clicked)
-        self.sine_button.toggled.connect(self.update_signal_options)
-        self.squareroot_button.toggled.connect(self.update_signal_options)
-
-        self.dac_sample_rate_button.toggled.connect(self.update_frequency_options)
-        self.dac_sample_rate_button.toggled.connect(self.get_frequency_info)
-        self.dac_sample_rate_button.toggled.connect(self.set_max_dac_steps)
-        self.dac_sample_rate_combobox.currentIndexChanged.connect(
-            self.get_frequency_info
-        )
-        self.signal_frequency_combobox.currentIndexChanged.connect(
-            self.get_frequency_info
-        )
-        self.signal_frequency_combobox.currentIndexChanged.connect(
-            self.set_max_dac_steps
-        )
-        self.dac_steps_edit.textChanged.connect(self.get_frequency_info)
-        self.dac_steps_edit.textChanged.connect(self.validate_dac_steps)
-        self.dac_sample_rate_button.toggled.connect(
-            self.validate_dac_steps_upon_button_toggled
-        )
-        self.signal_frequency_combobox.currentIndexChanged.connect(
-            self.validate_dac_steps_upon_button_toggled
-        )
-
-        self.sine_button.toggled.connect(self.update_dac_config)
-        self.squareroot_button.toggled.connect(self.update_dac_config)
-        self.sawtooth_button.toggled.connect(self.update_dac_config)
-        self.amplitude_edit.returnPressed.connect(self.update_dac_config)
-        self.start_voltage_edit.returnPressed.connect(self.update_dac_config)
-        self.stop_voltage_edit.returnPressed.connect(self.update_dac_config)
-        self.dac_sample_rate_combobox.currentIndexChanged.connect(
-            self.update_dac_config
-        )
-        self.dac_steps_edit.returnPressed.connect(self.update_dac_config)
-        self.signal_frequency_combobox.currentIndexChanged.connect(
-            self.update_dac_config
-        )
-        self.dac_sample_rate_button.toggled.connect(self.update_dac_config)
-        ###########################################################################
-
-        # Display settings as soon as the GUI starts
-        self.update_signal_options()
-        self.update_frequency_options()
-        self.get_frequency_info()
-        self.set_max_dac_steps()
-
     def get_reset_voltage(self):
         """get current reset voltage"""
+
         self.reset_voltage = float(self.reset_voltage_edit.text())
 
     @pyqtSlot()
     def sweep_button_clicked(self):
         self.start_sweep_button.toggle_button()
         if self.start_sweep_button.isChecked():
+            if self.adcreceiver.isRunning:
+                self.adcreceiver.stop()
+
             self.get_signal_info()
             self.get_frequency_info()
             initDacBram(
@@ -283,15 +231,21 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
         else:
             self.get_reset_voltage()
             stop_sweep(pita, self.channel, self.reset_voltage)
+        self.update_adc.emit()
 
     @pyqtSlot()
     def update_dac_config(self):
-        time.sleep(0.1)
+        self.update_signal_options()
+        self.update_frequency_options()
+        self.get_frequency_info()
+        self.show_valid_signal_frequency()
+
         if self.start_sweep_button.isChecked():
             if self.adcreceiver.isRunning():
                 self.adcreceiver.stop()
+
             self.get_signal_info()
-            self.get_frequency_info()
+            #self.get_frequency_info() #already implemented above 
             initDacBram(
                 pita,
                 self.signal_type,
@@ -302,17 +256,11 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
                 self.dwell_time_ms,
                 self.channel,
             )
-        self.restart_adc.emit()
+        self.update_adc.emit()
 
     def get_signal_info(self):
         """
         Get selected signal type and its parameters.
-
-        returns
-        -------
-        signal_type : string
-        start_V: float
-        stop_V : float
         """
 
         if self.sine_button.isChecked():
@@ -329,68 +277,47 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
                     self.start_V = float(self.start_voltage_edit.text())
                     self.stop_V = float(self.stop_voltage_edit.text())
 
-    def get_frequency_info(self):
-        """
-        Get dwell time and DAC steps to configure sample rate of DAC.
-
-        returns
-        -------
-        dac_steps : int
-        dwel_time_ms : float
-        """
+    def get_dac_steps(self):
         try:
             self.dac_steps = int(self.dac_steps_edit.text())
         except ValueError:
             return False
+
+    def show_valid_signal_frequency(self):
+        
+        self.signal_frequency_combobox.clear()  # Clear previous items
+        self.get_dac_steps()
+        valid_frequencies = []
+        for dac_sample_rate in self.dac_sample_rates:
+            valid_frequency = (dac_sample_rate * 1e6) / self.dac_steps
+            valid_frequencies.append(f"{valid_frequency:.2f} Hz")
+        self.signal_frequency_combobox.addItems(valid_frequencies)
+
+    def get_frequency_info(self):
+        """Get dwell time and DAC steps to configure sample rate of DAC."""
+
         if self.dac_sample_rate_button.isChecked():
             self.dac_sample_rate = float(
                 self.dac_sample_rate_combobox.currentText().split()[0]
             )
             self.dwell_time_ms = 1 / (self.dac_sample_rate * 1e3)
         elif self.signal_frequency_button.isChecked():
-            self.signal_frequency = float(
+            signal_frequency = float(
                 self.signal_frequency_combobox.currentText().split()[0]
             )
-            if self.signal_frequency == 2.5 or self.signal_frequency == 1.25:
-                self.dwell_time_ms = 1 / (self.signal_frequency * 1e3 * self.dac_steps)
-            else:
-                self.dwell_time_ms = 1 / (self.signal_frequency * self.dac_steps)
+
+            self.dwell_time_ms = 1000/(signal_frequency * self.dac_steps)
 
         self.show_dwell_time_ms_label.setText(str(self.dwell_time_ms))
 
     @pyqtSlot(str)
     def validate_dac_steps(self, text):
-        self.set_max_dac_steps()
-        if text and int(text) > self.max_dac_steps:
+        if text and int(text) > 16000:
             # Revert the input to the previous valid state
             cursor_position = self.dac_steps_edit.cursorPosition()
             self.dac_steps_edit.setText(text[:-1])
             self.dac_steps_edit.setCursorPosition(cursor_position - 1)
 
-    def set_max_dac_steps(self):
-        """
-        Shows the max. dac steps that can be given when certain signal frequency is desired
-        """
-
-        if self.dac_sample_rate_button.isChecked():
-            self.max_dac_steps = 2500
-        else:
-            signal_frequency = float(
-                self.signal_frequency_combobox.currentText().split()[0]
-            )
-            if signal_frequency == 2.5 or signal_frequency == 1.25:
-                self.max_dac_steps = int(MHZ_MAX_DAC_FREQ / signal_frequency)
-            else:
-                self.max_dac_steps = int(MHZ_MAX_DAC_FREQ / (signal_frequency / 1000))
-
-        self.show_max_dac_steps_label.setText(str(self.max_dac_steps))
-
-    @pyqtSlot()
-    def validate_dac_steps_upon_button_toggled(self):
-        if int(self.dac_steps_edit.text()) > int(self.show_max_dac_steps_label.text()):
-            self.dac_steps_edit.setText(str(self.show_max_dac_steps_label.text()))
-
-    @pyqtSlot()
     def update_signal_options(self):
         if self.sine_button.isChecked():
             self.amplitude_label.setVisible(True)
@@ -407,7 +334,6 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
             self.stop_voltage_label.setVisible(True)
             self.stop_voltage_edit.setVisible(True)
 
-    @pyqtSlot()
     def update_frequency_options(self):
         if self.dac_sample_rate_button.isChecked():
             self.dac_sample_rate_combobox.setVisible(True)
@@ -415,10 +341,3 @@ class DacBramSettingsTab(QGroupBox):  # QGroupBox
         else:
             self.dac_sample_rate_combobox.setVisible(False)
             self.signal_frequency_combobox.setVisible(True)
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = DacBramSettingsTab(1, None)
-    window.show()
-    sys.exit(app.exec())
